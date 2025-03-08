78fc8f14439a1746d7ec160e3dbe32bcd4f50a13
LassCissoko <lassanadu910@gmail.com>
2025-03-08 14:56:03 +0100

    Projet Terminé

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+
+const navBar = document.querySelector('#navbar');
+let currentScrollY = 0
+
+window.addEventListener('scroll', (e) => {
+
+  if (window.scrollY < currentScrollY) {
+    navBar.style.top = -160 + "px";
+    
+  } else {
+    navBar.style.top = 10 + "px";
+  }
+  currentScrollY = window.scrollY
+});
